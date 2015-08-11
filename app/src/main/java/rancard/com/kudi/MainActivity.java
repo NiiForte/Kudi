806f398040be020a1bf687c446b820ca61992446
“Kelvin <kelvin.bonni@gmail.com>
2015-08-11 16:47:42 +0000

    Added views for transaction list and account

--- app/src/main/java/rancard/com/kudi/MainActivity.java
+++ app/src/main/java/rancard/com/kudi/MainActivity.java
@@ -1,29 +1,31 @@
 package rancard.com.kudi;
 
-import java.util.Locale;
-
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.rancard.kudi.client.async.Callback;
 import com.rancard.kudi.client.async.Kudi;
 import com.rancard.kudi.domain.Account;
+import com.rancard.kudi.domain.Transaction;
 import com.rancard.kudi.domain.User;
 
-import fragment.CreateAccount;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -109,6 +111,12 @@ public class MainActivity extends AppCompatActivity {
                 case 1:
                     fragment = CreateAccount.newInstance(position + 1);
                     break;
+                case 2:
+                    fragment = ViewTransactionList.newInstance(position + 1);
+                    break;
+                case 3:
+                    fragment = ViewAccount.newInstance(position + 1);
+                    break;
                /* default:
                     fragment=CreateAccount.newInstance(position + 1);*/
 
@@ -119,8 +127,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 2;
+            // Show 4 total pages.
+            return 4;
         }
 
         @Override
@@ -198,10 +206,10 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
 
-                            userProfile = "First Name: " + user.getFirstName() + "/n";
-                            userProfile = userProfile + "Last Name: " + user.getLastName() + "/n";
-                            userProfile = userProfile + "Email: " + user.getEmail() + "/n";
-                            userProfile = userProfile + "Country" + user.getCountry() + "/n";
+                            userProfile = "First Name: " + user.getFirstName() + "\n";
+                            userProfile = userProfile + "Last Name: " + user.getLastName() + "\n";
+                            userProfile = userProfile + "Email: " + user.getEmail() + "\n";
+                            userProfile = userProfile + "Country" + user.getCountry() + "\n";
                             userProfile = userProfile + "Mobile Number: " + user.getMobileNumber();
                             name = user.getFirstName();
                             textView.setText(userProfile);
@@ -285,7 +293,6 @@ public class MainActivity extends AppCompatActivity {
                     });
 
 
-
                 }
 
             });
@@ -293,6 +300,156 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+
+        @Override
+        public void onResume() {
+            super.onResume();
+        }
+    }
+
+    public static class ViewAccount extends Fragment {
+
+        static Kudi kudiInstance = Kudi.newInstance("http://10.42.0.1:8080/wallet/api/v1");
+        static Kudi.Session session = kudiInstance.getSession("877");
+
+        static TextView accountName,accountNumber,accountTypeId;
+
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        String result;
+
+        public static ViewAccount newInstance(int sectionNumber) {
+            ViewAccount fragment = new ViewAccount();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+
+        public ViewAccount() {
+            // Required empty public constructor
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View rootView = inflater.inflate(R.layout.fragment_view_account, container, false);
+            accountNumber = (TextView) rootView.findViewById(R.id.accountNumber);
+            accountName = (TextView) rootView.findViewById(R.id.accountName);
+            accountTypeId = (TextView) rootView.findViewById(R.id.accountTypeId);
+            return rootView;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            Account account = new Account();
+            final Long accountNum = 123L;
+            session.viewAccount(accountNum, new Callback<Account>() {
+                @Override
+                public void onFailure(String s, int i) {
+                    Log.d("On Failure", s + " " + i);
+                }
+
+                @Override
+                public void onSuccess(Account account) {
+                    Log.d("On Success", account.toString());
+                    accountNumber.setText(account.getAccountNumber().toString());
+                    accountName.setText(account.getAccountName().toString());
+                    accountTypeId.setText(account.getAccountTypeId().toString());
+
+                }
+            });
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+        }
+    }
+
+    public static class ViewTransactionList extends Fragment {
+
+        static Kudi kudiInstance = Kudi.newInstance("http://10.42.0.1:8080/wallet/api/v1");
+        static Kudi.Session session = kudiInstance.getSession("877");
+
+        ListView transactionList;
+
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment ViewTransactionList.
+         */
+        public static ViewTransactionList newInstance(int sectionNumber) {
+            ViewTransactionList fragment = new ViewTransactionList();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public ViewTransactionList() {
+            // Required empty public constructor
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View rootView = inflater.inflate(R.layout.fragment_view_transaction_list, container, false);
+
+            transactionList = (ListView) rootView.findViewById(R.id.transact_list);
+
+            return rootView;
+        }
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            Transaction.Query query = new Transaction.Query();
+            query.setAccount(1234L);
+            session.viewTransactionList(query, new Callback<List<Transaction>>() {
+                @Override
+                public void onFailure(String s, int i) {
+
+                    Log.d("++++++++++++++++++++", s);
+
+                }
+
+                @Override
+                public void onSuccess(final List<Transaction> transactions) {
+
+                    mActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d("TransactionList", transactions.toString());
+
+
+                            ArrayList<Transaction> transactionArrayList = (ArrayList<Transaction>) transactions;
+
+                            ArrayAdapter<Transaction> adapter = new ArrayAdapter<>(mActivity.getApplicationContext(),
+                                    android.R.layout.simple_list_item_1, transactionArrayList);
+
+                            transactionList.setAdapter(adapter);
+//                            adapter.getView();
+
+                        }
+                    });
+
+                }
+
+            });
+
+        }
 
         @Override
         public void onResume() {
